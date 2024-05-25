69082e708a87fdfa5f76fd69f82e64ef12ebd918
Subham-dury <karchowdhurysubham@gmail.com>
2024-05-25 14:03:24 +0530

    Adding java project

---
+++ base-proj3/src/main/java/com/example/base_proj3/BaseProj3Application.java
@@ -0,0 +1,13 @@
+package com.example.base_proj3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseProj3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseProj3Application.class, args);
+	}
+
+}
---
+++ base-proj3/src/test/java/com/example/base_proj3/BaseProj3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.base_proj3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseProj3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
